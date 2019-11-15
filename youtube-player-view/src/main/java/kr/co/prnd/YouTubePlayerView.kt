b496cde67c4a2dbccfb1a68027f4a16967587c2c
ted-prnd <ted@prnd.co.kr>
2019-11-15 10:55:53 +0900

    Support use YouTubePlayerView in fragment

--- youtube-player-view/src/main/java/kr/co/prnd/YouTubePlayerView.kt
+++ youtube-player-view/src/main/java/kr/co/prnd/YouTubePlayerView.kt
@@ -1,10 +1,13 @@
 package kr.co.prnd
 
+import android.app.Activity
 import android.content.Context
+import android.content.ContextWrapper
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.FrameLayout
 import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
 import com.google.android.youtube.player.YouTubeInitializationResult
 import com.google.android.youtube.player.YouTubePlayer
 import com.google.android.youtube.player.YouTubePlayerAndroidxFragment
@@ -17,28 +20,66 @@ class YouTubePlayerView @JvmOverloads constructor(
 ) : FrameLayout(context, attrs, defStyle) {
 
     private var videoId: String? = null
+    private var fragmentName: String? = null
     var onInitializedListener: OnInitializedListener? = null
+    private val fragmentManager: FragmentManager
+    private val youTubePlayerAndroidxFragment = YouTubePlayerAndroidxFragment()
 
     init {
+
         LayoutInflater.from(context).inflate(R.layout.view_prnd_you_tube_player, this, true)
 
         attrs?.let {
             val typedArray =
                 context.obtainStyledAttributes(attrs, R.styleable.YouTubePlayerView, defStyle, 0)
             videoId = typedArray.getString(R.styleable.YouTubePlayerView_videoId)
+            fragmentName = typedArray.getString(R.styleable.YouTubePlayerView_fragment)
             typedArray.recycle()
         }
 
+        fragmentManager = getFragmentManager()
+        fragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, youTubePlayerAndroidxFragment)
+            .commitAllowingStateLoss()
 
         if (videoId != null) {
             play(videoId!!)
         }
     }
 
+    private fun getFragmentManager(): FragmentManager {
+        val activityFragmentManager = getFragmentActivity().supportFragmentManager
+        if (fragmentName == null) {
+            return activityFragmentManager
+        } else {
+            for (fragment in activityFragmentManager.fragments) {
+                if (fragment.javaClass.name == fragmentName) {
+                    return fragment.childFragmentManager
+                }
+            }
+            throw IllegalArgumentException("[$fragmentName] can not found. Please check your fragment name")
+        }
+    }
+
+    private fun getFragmentActivity(): FragmentActivity {
+        var targetContext = context
+        var targetActivity: Activity? = null
+        while (targetContext is ContextWrapper) {
+            if (targetContext is Activity) {
+                targetActivity = targetContext
+                break
+            }
+            targetContext = targetContext.baseContext
+        }
+        return (targetActivity as? FragmentActivity)
+            ?: throw IllegalArgumentException("You have to extend FragmentActivity or AppCompatActivity")
+    }
+
+
     fun play(videoId: String, listener: OnInitializedListener? = null) {
         listener?.let { this.onInitializedListener = it }
 
-        getYouTubeFragment()?.initialize(
+        youTubePlayerAndroidxFragment.initialize(
             javaClass.simpleName,
             object : YouTubePlayer.OnInitializedListener {
                 override fun onInitializationSuccess(
@@ -60,12 +101,6 @@ class YouTubePlayerView @JvmOverloads constructor(
                     onInitializedListener?.onInitializationFailure(provider, result)
                 }
             })
-    }
-
-    private fun getYouTubeFragment(): YouTubePlayerAndroidxFragment? {
-        val fragmentManager = (context as? FragmentActivity)?.supportFragmentManager
-        return fragmentManager?.findFragmentByTag(context.getString(R.string.prnd_youtube_player_view))
-                as? YouTubePlayerAndroidxFragment
     }
 
     interface OnInitializedListener {
